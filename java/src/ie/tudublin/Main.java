a09998e3907754b053aaea92643cbe110b9c60eb
JX <user@Siong-MacBook-Pro.local>
2022-05-04 16:14:12 +0100

    drumset

--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -10,7 +10,7 @@ public class Main {
 
 	public void startUI() {
 		String[] a = { "MAIN" };
-		processing.core.PApplet.runSketch(a, new DrumSet());
+		processing.core.PApplet.runSketch(a, new Menu());
 	}
 
 	public static void main(String[] args) {
